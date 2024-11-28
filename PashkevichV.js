b00fa26995671d9599e876f82a51f5366289a213
ReimaiChan <148211904+ReimaiChan@users.noreply.github.com>
2024-11-28 16:00:58 +0300

    Update PashkevichV.js

--- PashkevichV.js
+++ PashkevichV.js
@@ -1,5 +1,5 @@
 document.getElementById('menu').onclick = menuclose;
-document.getElementById('openmenu').onclick = menuclose;
+document.getElementById('openmenu').onclick = menuopen;
 function menuclose() {
          document.getElementById('menu').hidden = true
          document.getElementById('openmenu').innerHTML = "Меню"
