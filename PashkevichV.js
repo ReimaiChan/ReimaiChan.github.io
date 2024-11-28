76254345c88bce22c83ba228e16c463d04d16418
ReimaiChan <148211904+ReimaiChan@users.noreply.github.com>
2024-11-28 15:55:43 +0300

    Update PashkevichV.js

--- PashkevichV.js
+++ PashkevichV.js
@@ -1,4 +1,9 @@
 document.getElementById('menu').onclick = menuclose;
 function menuclose() {
          document.getElementById('menu').hidden = true
+         document.getElementById('openmenu').innerHTML = "Меню"
+      }
+function menuopen() {
+         document.getElementById('menu').hidden = false
+         document.getElementById('openmenu').hidden = true
       }
